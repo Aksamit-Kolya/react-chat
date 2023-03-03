cade80bd62e7a32b582ca8de11cd6f5682c6d05d
Andrei_pivavarchyk_mindsquare <andrei.pivavarchyk.extern@mindsquare.de>
2023-03-03 15:06:14 +0300

    draft version 2 of the chat

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,28 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./ChatPage.css";
+import arrowImage from "./arrow.png";
 
-function ChatPage() {
-  const [messages, setMessages] = useState([
-    { id: 1, author: "John", userId: 1, text: "Hello, how are you?" },
-    { id: 2, author: "Mary", userId: 2, text: "I'm doing well, thanks for asking." },
-    { id: 3, author: "John", userId: 1, text: "What have you been up to lately?" },
-  ]);
-  const [inputValue, setInputValue] = useState("");
+const ChatPage = ({ user }) => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const messagesEndRef = useRef(null);
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
+  useEffect(() => {
+    // simulate fetching messages from a server
+    setTimeout(() => {
+      setMessages([
+        {
+          id: 1,
+          author: "John",
+          text: "Hello!",
+        },
+        {
+          id: 2,
+          author: user,
+          text: "Hi there!",
+        },
+        {
+          id: 3,
+          author: "John",
+          text: "How are you doing?",
+        },
+      ]);
+    }, 1000);
+  }, [user]);
 
-  const handleSubmit = (event) => {
+  useEffect(() => {
+    // scroll to the bottom of the message container when new messages are added
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  const handleMessageSubmit = (event) => {
     event.preventDefault();
-    const newMessage = {
-      id: messages.length + 1,
-      author: "User",
-      userId: 0,
-      text: inputValue,
+    const newId = messages.length + 1;
+    const newMessageObject = {
+      id: newId,
+      author: user,
+      text: newMessage,
     };
-    setMessages([...messages, newMessage]);
-    setInputValue("");
+    setMessages([...messages, newMessageObject]);
+    setNewMessage("");
   };
 
   return (
@@ -31,23 +53,32 @@ function ChatPage() {
         {messages.map((message) => (
           <div
             key={message.id}
-            className={`message ${
-              message.userId === 0 ? "user-message" : "other-message"
-            }`}
+            className={
+              message.author === user ? "message user-message" : "message other-message"
+            }
           >
-            <div className="message-author">{message.author} ({message.userId})</div>
             <div className="message-text-container">
+              <div className="message-author">{message.author}</div>
               <div className="message-text">{message.text}</div>
             </div>
           </div>
         ))}
+        <div ref={messagesEndRef} />
       </div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button type="submit">Send</button>
+      <form onSubmit={handleMessageSubmit}>
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={newMessage}
+          onChange={(event) => setNewMessage(event.target.value)}
+          className="message-input"
+        />
+        <button type="submit" className="send-button">
+          <img src={arrowImage} alt="Send" className="send-button-image" />
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default ChatPage;
