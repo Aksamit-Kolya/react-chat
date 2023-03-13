c54b4f06634ff6024b9a2a4d25a558fee3138dcd
Andrei_pivavarchyk_mindsquare <andrei.pivavarchyk.extern@mindsquare.de>
2023-03-13 21:38:11 +0300

    before refactoring(incomplete editing feature)

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -5,6 +5,8 @@
   import * as StompJs from "@stomp/stompjs";
   import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
   import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import TextField from '@mui/material/TextField';
+
 
   const ChatPage = ({ user }) => {
     const [messages, setMessages] = useState([]);
@@ -30,6 +32,7 @@
     const prevMessages = usePreviousValue(messages);
     const [selectedMessage, setSelectedMessage] = useState(null);
     const [contextMenuPosition, setContextMenuPosition] = useState({ top: 0, left: 0 });
+    const [editingMessage, setEditingMessage] = useState(null);
 
     function usePreviousValue(value) {
       const ref = useRef();
@@ -44,7 +47,8 @@
       
       setLoading(true);
       const messagesContainer = document.querySelector(".messages-container");
-      
+
+      console.log('findHistory(1)');
       ChatService.findHistory(Math.floor(messages.length / loadingMessagesPageSize), loadingMessagesPageSize).then((response) => {
         customSetMessages([...response.data.slice(0, Math.min(loadingMessagesPageSize - messages.length % loadingMessagesPageSize, response.data.length - messages.length % loadingMessagesPageSize)), ...messages]);
         messagesContainer.scrollTop = 43.3 * loadingMessagesPageSize * (loadingMessagesPageSize - messages.length % loadingMessagesPageSize) / loadingMessagesPageSize;
@@ -59,6 +63,7 @@
     };
 
     useEffect(() => {
+      console.log('### Conncet');
       const client = new StompJs.Client({
         brokerURL: "ws://localhost:8080/actions",
         debug: function (str) {
@@ -95,6 +100,7 @@
 
 
     useEffect(() => {
+      console.log('findHistory(2)');
       ChatService.findHistory(0, 20).then(response => {
         
         customSetMessages(response.data.map((element) => {return { ...element, login: element.login }})); ///setMessages(response.data));
@@ -141,6 +147,7 @@
 
     const handleUpdateMessage = (event) => {
       event.preventDefault();
+      setEditingMessage(selectedMessage);
       // const updatedMessages = messages.filter((message) => message.id !== selectedMessage.id);
       // customSetMessages(updatedMessages);
       // setSelectedMessage(null);
@@ -156,8 +163,21 @@
       }
     }
 
+    function handleChatContainerClick(event) {
+      if(!event.target.closest('.message-box')) {
+        setSelectedMessage(null);
+      }
+      if(!event.target.closest('.edit-message-input')){
+        setEditingMessage(null);
+      }
+    }
+
+    function handleEditFormClick(event) {
+      ///event.stopPropagation();
+    }
+
     return (
-      <div className="chat-container" onClickCapture={() => setSelectedMessage(null)}>
+      <div className="chat-container" onClick={handleChatContainerClick}>
         <div className="messages-container" onScroll={handleMessagesContainerScroll}>
           {
             messages.map((message) => (
@@ -190,18 +210,54 @@
           )}
           <div ref={messagesEndRef} />
         </div>
-        {selectedMessage && (
-          <div className="context-menu" style={{position: "absolute", top: contextMenuPosition.top, left: contextMenuPosition.left }}>
-            <button onClickCapture={handleUpdateMessage}>
-              <FontAwesomeIcon icon={faEdit} className="context-menu-icon" />
-              Update
-            </button>
-            <button onClickCapture={handleDeleteMessage}>
-              <FontAwesomeIcon icon={faTrash} className="context-menu-icon" />
-              Delete
-            </button>
-          </div>
-        )}
+        {selectedMessage && !editingMessage ? 
+          (
+            <div className="context-menu" style={{position: "absolute", top: contextMenuPosition.top, left: contextMenuPosition.left }}>
+              <button onClickCapture={handleUpdateMessage}>
+                <FontAwesomeIcon icon={faEdit} className="context-menu-icon" />
+                Edit
+              </button>
+              <button onClickCapture={handleDeleteMessage}>
+                <FontAwesomeIcon icon={faTrash} className="context-menu-icon" />
+                Delete
+              </button>
+            </div>
+          )
+        :
+          editingMessage && (
+            <form className="edit-message-form" onClickCapture={handleEditFormClick} onSubmit={handleUpdateMessage} style={{top: contextMenuPosition.top, left: contextMenuPosition.left}}>
+              <div id="input" contenteditable data-placeholder="Enter your text"></div>
+              <TextField
+                className="edit-message-input"
+                multiline
+                variant="outlined"
+                sx={{
+                  "& .MuiOutlinedInput-root.Mui-focused": {
+                    "& > fieldset": {
+                      borderColor: "black",
+                      borderWidth: 1
+                    }
+                  }
+                }}
+                InputProps={{
+                  style: {
+                    borderRadius: '5px',
+                    margin: '5px',
+                    width: '135%',
+                    height: '10px',
+                    fontSize: '12px',
+                  }
+                }}
+              />
+              <div className="edit-message-buttons-container">
+                <button type="submit">Edit</button>
+                <button type="button" onClick={() => setEditingMessage(null)}>
+                  Cancel
+                </button>
+              </div>
+            </form>
+          )
+        }
         <hr className="divider"/>
         <form onSubmit={handleMessageSubmit} className="message-input-container">
           <input
